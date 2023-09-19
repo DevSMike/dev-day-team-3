68e792e05288f42b7f27d2cf9c37a56ee5c7f8db
Mike Lukashev <boyskyfall@vk.com>
2023-09-19 15:05:52 +0300

    feat: class accepted

--- src/main/java/com/team3/holiday/review/MathCore.java
+++ src/main/java/com/team3/holiday/review/MathCore.java
@@ -2,17 +2,23 @@ package com.team3.holiday.review;
 
 import java.util.Arrays;
 
+//review made
 public class MathCore {
-    private static final char[] chars = {
+
+    private static final char[] CHARS = {
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
             'A', 'B', 'C', 'D', 'E', 'F',
             'a', 'b', 'c', 'd', 'e', 'f'
     };
+    
+   private static final char[] ANSWER = { '5', '5', 'C', 'f', 'c', 'e', 'f', '9'};
 
-    public static String hackPass() {
+
+    public  String hackPass() {
         long a = 0L;
         long b = passToLong("ffffffff");
         long m = (a + b) / 2;
+
         while (true) {
             int response = tryPass(longToPass(m));
             if (response == 1) {
@@ -30,33 +36,45 @@ public class MathCore {
         return longToPass(m);
     }
 
-    private static int tryPass(String password) {
+    private  int tryPass(String password) {
+        char[] charPass = password.toCharArray();
+
+        for (int i = 0; i < password.length(); i++) {
+            if (charPass[i] > ANSWER[i]) {
+                return 1;
+            } else if (charPass[i] < ANSWER[i]) {
+                return -1;
+            }
+
+        }
         // вернет 1, если >pass
         // вернет -1, если <pass
         // вернет 0, если code 200
         return 0;
     }
 
-    private static long passToLong(String password) {
+    private  long passToLong(String password) {
 
         int len = password.length();
         long res = 0L;
         long pow = 1;
         for (int i = len - 1; i >= 0; i--) {
-            res += Arrays.binarySearch(chars, password.charAt(i)) * pow;
+            res += Arrays.binarySearch(CHARS, password.charAt(i)) * pow;
             pow *= 22;
         }
         return res;
     }
 
-    private static String longToPass(long number) {
+    private  String longToPass(long number) {
         int r = 0;
         StringBuilder res = new StringBuilder();
         while (number > 0) {
-            r = (int) number % 22;
-            res.insert(0, chars[r]);
+
+            r = (int) (number % 22);
+            res.insert(0, CHARS[r]);
             number = number / 22;
         }
         return res.toString();
     }
+
 }
