9d4dce206f9a3367704c4c982de01f7bdd275a10
DevSMike <likashev.migha@yandex.ru>
2023-09-21 18:10:58 +0300

    feat: final refactor code

--- src/main/java/com/team3/holiday/service/password/PasswordGenerationServiceImpl.java
+++ src/main/java/com/team3/holiday/service/password/PasswordGenerationServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.Arrays;
 
 @Service
-public class PasswordGenerationServiceImpl implements PasswordGenerationService{
+public class PasswordGenerationServiceImpl implements PasswordGenerationService {
 
     private static final char[] CHARS = {
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
