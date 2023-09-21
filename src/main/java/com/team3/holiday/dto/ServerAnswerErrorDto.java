b6c995f2a69e3dce3ffeaba7b87856d264316698
DevSMike <likashev.migha@yandex.ru>
2023-09-21 13:04:22 +0300

    feat: update server error class

--- src/main/java/com/team3/holiday/dto/ServerAnswerErrorDto.java
+++ src/main/java/com/team3/holiday/dto/ServerAnswerErrorDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,5 +14,5 @@ public class ServerAnswerErrorDto {
 
     private Boolean completed;
     private String errorMessage;
-    private int codeError;
+    private HttpStatus codeError;
 }
